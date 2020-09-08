e3cc0992fb140c1266967e4f7099c5a9826522d6
Thijn Albers <thijn.albers@gmail.com>
2020-09-08 13:05:10 +0200

    Minor Changes

--- Lab_1/Thijn/app/Lab1.hs
+++ Lab_1/Thijn/app/Lab1.hs
@@ -30,7 +30,7 @@ boys = [Matthew, Peter, Jack, Arnold, Carl]
 genPositiveIntegers :: Gen Integer
 genPositiveIntegers = abs <$> (arbitrary :: Gen Integer) `suchThat` (> 0)
 
--- Ex 1
+-- Ex 1 (1.5 hours)
 naturals :: [Integer]
 naturals = [1..]
 
@@ -49,10 +49,10 @@ quickCheckSquarelst = quickCheckResult $ forAll genPositiveIntegers testSquarels
 
 -- Workshop 1.3
 cubelst :: Integer -> Integer
-cubelst x = foldr (\x acc ->  acc + x^3) 0 [1..n] where n = abs x
+cubelst n = foldr (\x acc ->  acc + x^3) 0 [1..n]
 
 cubelst' :: Integer -> Integer
-cubelst' x = div (n ^ 2 + n) 2 ^ 2 where n = abs x
+cubelst' n = div (n ^ 2 + n) 2 ^ 2
 
 testCubelst :: Integer -> Bool
 testCubelst n = cubelst n == cubelst' n
@@ -60,7 +60,7 @@ testCubelst n = cubelst n == cubelst' n
 quickCheckCubelst :: IO Result
 quickCheckCubelst = quickCheckResult $ forAll genPositiveIntegers testCubelst
 
--- Ex 2
+-- Ex 2 (30 minutes)
 prop :: [Integer] -> Bool
 prop a = 2 ^ length a == lengthPwrset a
 
@@ -70,11 +70,11 @@ lengthPwrset a = toInteger (length (subsequences a))
 testPwrset :: [Integer] -> Bool
 testPwrset = prop
 
--- Note: will run for a long time
+-- Note: will run for a long time due to the exponential complexity
 quickCheckPwrset :: IO ()
 quickCheckPwrset = quickCheck testPwrset
 
--- Ex 3
+-- Ex 3 (30 minutes)
 lengthPerms :: [Integer] -> Integer
 lengthPerms = product
 
@@ -87,17 +87,17 @@ testLengthPerms a = lengthPerms a == lengthPerms' a
 quickCheckLengthPerms :: IO ()
 quickCheckLengthPerms = quickCheck testLengthPerms
 
--- Ex 4
+-- Ex 4 (20 minutes)
 reversalPrimes :: [Integer]
 reversalPrimes = filter (\x -> prime x && prime (reversal x)) [1..10000]
 
--- Ex 5
+-- Ex 5 (10 minutes)
 -- Returns 24680
 -- Which is the sum of the first 101 primes
 smallestPrime :: Integer
 smallestPrime = sum (take 101 primes)
 
--- Ex 6
+-- Ex 6 (30 minutes)
 ex6Calculation :: Integer -> Integer
 ex6Calculation n = product (take (fromIntegral n) primes) + 1
 
@@ -108,7 +108,7 @@ testEx6Calculation n = not (prime (ex6Calculation n))
 counterConjecture' :: [Integer]
 counterConjecture' = filter testEx6Calculation primes
 
--- Ex 7
+-- Ex 7 (1.5 hours)
 -- Split integer into a list of its digits
 digits :: Integer -> [Integer]
 digits 0 = []
@@ -121,7 +121,7 @@ reverseIndexed bs = reverse (zip [0 .. ] (reverse bs))
 -- Double every other element starting from the first position left the check (last) digit
 -- By reverse indexing the list we can multiply every element at uneven index
 doubled :: [Integer] -> [Integer]
-doubled xs =  map (\(i, x) -> if not (even i) then x * 2 else x) (reverseIndexed xs)
+doubled xs =  map (\(i, x) -> if odd i then x * 2 else x) (reverseIndexed xs)
 
 sumDigits :: [Integer] -> [Integer]
 sumDigits xs = map (\x -> if x > 9 then x - 9 else x) (doubled xs)
