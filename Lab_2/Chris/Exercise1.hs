d45f9af3f4422ebc5a12b353923d777e80aa4ab5
Cristian Tamas <cristiantamas05@gmail.com>
2020-09-15 08:56:04 +0200

    Exercises 1 and 2

--- Lab_2/Chris/Exercise1.hs
+++ Lab_2/Chris/Exercise1.hs
@@ -1,44 +1,35 @@
-module Lab1 where
-import Test.QuickCheck 
+import Lab2 ( Shape(..), (-->), probs )
+import Data.List
 
--- Time spent: 1h
--- Workshop 1, Exercise 2
-
-square_function :: Integer -> Integer
-square_function x = x * x 
-
-sum_of_squares' :: Integer -> Integer
-sum_of_squares' n = sum(map square_function [1..n])
-
-sum_of_squares :: Integer -> Integer
-sum_of_squares n = (n * (n + 1) * (2 * n + 1)) `div` 6
-
-test_sum_of_squares :: Integer -> Bool
-test_sum_of_squares n = let a = abs n in sum_of_squares' a == sum_of_squares a 
+-- Time spent: 2h 30min
 
 
+distibution :: Float -> String
+distibution n 
+    | n > 0 && n < 0.25 = "Q1"
+    | n >= 0.25 && n < 0.5 = "Q2"
+    | n >= 0.5 && n < 0.75 = "Q3"
+    | n >= 0.75 && n < 1 = "Q4"
 
--- Workshop 1, Exervise 3
+find_quarters :: [Float] -> [String]
+find_quarters = map distibution
 
-cube_function :: Integer -> Integer
-cube_function x = x * x * x 
+find_quarters_monad :: IO [Float] -> IO [String]
+find_quarters_monad = fmap find_quarters
 
-sum_of_cubes' :: Integer -> Integer
-sum_of_cubes' n = sum(map cube_function [1..n])
-
-sum_of_cubes :: Integer -> Integer
-sum_of_cubes n = square_function (n * (n + 1) `div` 2)
-
-test_sum_of_cubes :: Integer -> Bool
-test_sum_of_cubes n = let a = abs n in sum_of_cubes' a == sum_of_cubes a 
-
-
--- Main 
+count_quarters :: [String] -> String -> Int
+count_quarters xs s = length (filter (== s) xs)
 
 main :: IO()
 main = do
-    putStrLn "Proof induction Exercise 2"
-    quickCheck test_sum_of_squares
-
-    putStrLn "Proof induction Exervise 3 "
-    quickCheck test_sum_of_cubes
+    generated <- find_quarters_monad (probs 10000)
+    putStrLn "Distribution"
+    putStr "Q1= "
+    print (count_quarters generated "Q1")
+    putStr "Q2= "
+    print (count_quarters generated "Q2")
+    putStr "Q3= "
+    print (count_quarters generated "Q3")
+    putStr "Q4= "
+    print (count_quarters generated "Q4")
+    
